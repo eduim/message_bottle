15ae585470d8d3b65ae95c59e6570f6303255a08
eduim <eduardo.im92@gmail.com>
2022-10-11 18:02:27 +0200

    feat/koa server working

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "957cfdf4c6792b5d"; }
+/******/ 		__webpack_require__.h = function() { return "57a6e67cf5c214b7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
