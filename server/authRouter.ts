56d91172deb73c793c9e7eab20fc12e1b25f4dbe
eduim <eduardo.im92@gmail.com>
2022-10-15 10:23:54 +0200

    refactor: moods controller changed to authrouter

--- server/authRouter.ts
+++ server/authRouter.ts
@@ -1,8 +1,11 @@
 import Router from 'koa-router';
+import MoodsController from '../controllers/moods.controller';
 import UsersController from './controllers/Users.controller';
 import authMiddleware from './middlewares/authentication';
+
 const authRouter = new Router();
 
 authRouter.get('/me', authMiddleware, UsersController.authentication);
+authRouter.post('/moods', MoodsController.createMood);
 
 export default authRouter;
