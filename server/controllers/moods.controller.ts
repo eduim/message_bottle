4ec30527523cc51fea81fa1b64aaae3ec70f55be
bienmag <mvaretckaia@gmail.com>
2022-10-17 11:48:18 +0200

    POST/moods: userId attached to the mood

--- server/controllers/moods.controller.ts
+++ server/controllers/moods.controller.ts
@@ -3,22 +3,25 @@ import Koa from 'koa';
 
 const MoodsController = {
   async createMood(ctx: Koa.Context, next: Koa.Next) {
+    console.log('hello', ctx.user);
     if (
       ctx.request.body === undefined ||
       typeof ctx.request.body?.mood !== 'number'
     ) {
       ctx.status = 400;
       ctx.body = {
-        error: 'Missing data in request.',
+        error: 'Missing data in request.'
       };
       return;
     }
-    const { mood } = ctx.request.body;
 
-    const record = await Mood.create(mood);
+    const { mood } = ctx.request.body;
+    const userId = ctx.user.id;
+
+    const record = await Mood.create(mood, userId);
     ctx.statusCode = 201;
     ctx.response.body = record;
-  },
+  }
 };
 
 export default MoodsController;
