752c91feb3941aa48d2ddb6a00c6de955ca381c1
eduim <eduardo.im92@gmail.com>
2022-10-17 10:36:26 +0200

    feat: button github

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,12 +3,15 @@ import LoginButton from '../components/LoginButton';
 
 export default function login(): JSX.Element {
   return (
-    <div className={styles.container}>
-      <div className={styles.main}>
-        <h1 className={styles.title1}> JOIN US!</h1>
-        <div className={styles.submitPart}>
-          <LoginButton href="login/github">Log in</LoginButton>
+    <div className={styles.main}>
+      <h1 className={styles.title}>
+        Welcome to <a>Message in a Bottle!</a>
+      </h1>
+      <div className={styles.grid}>
+        <div className={styles.moodTitle}>
+          <h1>Login with Github</h1>
         </div>
+        <LoginButton href="login/github">Log in</LoginButton>
       </div>
     </div>
   );
