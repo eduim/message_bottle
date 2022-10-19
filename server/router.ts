6666a97f8f639b30ed9a811b2ba0fc811fad7996
Martina Cumellas <cumellasmartina@gmail.com>
2022-10-19 11:16:49 +0200

    feat: get moods

--- server/router.ts
+++ server/router.ts
@@ -1,5 +1,6 @@
 import Router from 'koa-router';
 import MessagesController from './controllers/Messages.controller';
+import MoodsController from './controllers/Moods.controller';
 import UsersController from './controllers/Users.controller';
 
 const router = new Router();
@@ -7,5 +8,7 @@ const router = new Router();
 router.get('/login/github', UsersController.requestAuthorization);
 router.get('/login/github/callback', UsersController.getAuthorization);
 router.post('/messages', MessagesController.postMessage);
+router.post('/moods', MoodsController.createMood);
+router.get('/moods', MoodsController.getMoods);
 
 export default router;
