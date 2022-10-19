ee4c03340259265c8e703143193475b3bdf98432
Martina Cumellas <cumellasmartina@gmail.com>
2022-10-19 11:57:30 +0200

    fix: move mood routes behind auth router

--- server/router.ts
+++ server/router.ts
@@ -1,14 +1,9 @@
 import Router from 'koa-router';
-import MessagesController from './controllers/Messages.controller';
-import MoodsController from './controllers/Moods.controller';
 import UsersController from './controllers/Users.controller';
 
 const router = new Router();
 
 router.get('/login/github', UsersController.requestAuthorization);
 router.get('/login/github/callback', UsersController.getAuthorization);
-router.post('/messages', MessagesController.postMessage);
-router.post('/moods', MoodsController.createMood);
-router.get('/moods', MoodsController.getMoods);
 
 export default router;
