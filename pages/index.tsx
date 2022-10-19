cac927b8ca90b8a7297695de794c1df04abcbf3e
Martina Cumellas <cumellasmartina@gmail.com>
2022-10-19 11:15:57 +0200

    feat: add wave to the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home: NextPage = () => {
   async function postMood(id: number): Promise<void> {
     try {
       await api.post('/moods', {
-        mood: id
+        mood: id,
       });
       void (await router.push('/getorpost'));
     } catch (e: any) {
@@ -80,9 +80,21 @@ const Home: NextPage = () => {
         </div>
       </main>
       <div className={styles.brandBox}>
-        <a href="/login">
-          <img src="/assets/logo.png" width={75} height={75} />
-        </a>
+        <img src="/assets/logo.png" width={75} height={75} />
+        <svg className={styles.wave} viewBox="0 0 12960 1120">
+          <path d="M9720,320C8100,320,8100,0,6480,0S4860,320,3240,320,1620,0,0,0V1120H12960V0C11340,0,11340,320,9720,320Z">
+            <animate
+              dur="5s"
+              repeatCount="indefinite"
+              attributeName="d"
+              values="
+              M9720,320C8100,320,8100,0,6480,0S4860,320,3240,320,1620,0,0,0V1120H12960V0C11340,0,11340,320,9720,320Z;
+              M9720,0C8100,0,8100,319,6480,319S4860,0,3240,0,1620,320,0,320v800H12960V320C11340,320,11340,0,9720,0Z;
+              M9720,320C8100,320,8100,0,6480,0S4860,320,3240,320,1620,0,0,0V1120H12960V0C11340,0,11340,320,9720,320Z
+            "
+            />
+          </path>
+        </svg>
       </div>
       {/* 
       <footer className={styles.footer}>
