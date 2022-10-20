490935daa3d0e41cd9a4322fb56bbc17d354e099
Martina Cumellas <cumellasmartina@gmail.com>
2022-10-20 16:23:14 +0200

    fix: delete wrong routers

--- server/router.ts
+++ server/router.ts
@@ -1,12 +1,9 @@
 import Router from 'koa-router';
-import MoodsController from './controllers/moods.controller';
 import UsersController from './controllers/Users.controller';
 
 const router = new Router();
 
 router.get('/login/github', UsersController.requestAuthorization);
 router.get('/login/github/callback', UsersController.getAuthorization);
-router.post('/moods', MoodsController.createMood);
-router.get('/moods', MoodsController.getMoods);
 
 export default router;
