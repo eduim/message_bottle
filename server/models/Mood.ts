74d27a0d628f260d6e27cbc4386305e55d8c6579
eduim <eduardo.im92@gmail.com>
2022-10-20 16:31:55 +0200

    Update Mood.ts

--- server/models/Mood.ts
+++ server/models/Mood.ts
@@ -1,9 +1,5 @@
 import prisma from '../lib/prisma';
-<<<<<<< HEAD
-import User from './users';
-=======
 import User from './Users';
->>>>>>> d914a1f0c217dcc2caf3cba0687de2c65fb451f5
 
 class Mood {
   constructor(
@@ -28,11 +24,11 @@ class Mood {
     return new Mood(id, mood, postDate);
   }
 
-  static async getCurrentMood(userId: number): Promise<any> {
+  static async getCurrentMood(userId: number): Promise<Mood | null> {
     const day = Date.now() - 24 * 60 * 60 * 1000;
     const lastDay = new Date(day).toISOString();
 
-    const lastMood = await prisma.mood.findMany({
+    const moods = await prisma.mood.findMany({
       where: {
         AND: [
           {
@@ -51,44 +47,20 @@ class Mood {
       take: 1,
     });
 
-    return lastMood;
+    if (moods[0] === undefined) return null;
+
+    return moods[0];
   }
 
   static async checkTodayMood(userId: number): Promise<boolean> {
-    const day = Date.now() - 24 * 60 * 60 * 1000;
-    const lastDay = new Date(day).toISOString();
-    try {
-      const lastMood = await prisma.mood.findMany({
-        where: {
-          AND: [
-            {
-              userId,
-            },
-            {
-              postDate: {
-                gte: lastDay,
-              },
-            },
-          ],
-        },
-        orderBy: {
-          postDate: 'desc',
-        },
-        take: 1,
-      });
-
-      const currentDay = new Date(Date.now()).getDate();
-      return lastMood[0].postDate.getDate() === currentDay;
-    } catch {
-      return false;
-    }
+    return (await this.getCurrentMood(userId)) !== null;
   }
 
   static async getMood(): Promise<any> {
     const result = await prisma.$queryRaw`
     SELECT
       date_trunc('day', "public"."Mood"."postDate") as postDate,
-      ROUND(AVG("public"."Mood"."mood"),2) as av_mood
+      ROUND(AVG("public"."Mood"."mood"),2) as mood
     FROM
       "public"."Mood"
     GROUP BY 1
